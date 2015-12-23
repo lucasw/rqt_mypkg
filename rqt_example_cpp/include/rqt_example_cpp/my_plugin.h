17c7ef7634104c76f93b0158f5add1796e1c1bcd
Lucas Walter <wsacul@gmail.com>
2015-12-23 07:51:27 -0800

    Now have example C++ plugin

---
+++ rqt_example_cpp/include/rqt_example_cpp/my_plugin.h
@@ -0,0 +1,29 @@
+#ifndef rqt_example_cpp_my_plugin_H
+#define rqt_example_cpp_my_plugin_H
+
+#include <rqt_gui_cpp/plugin.h>
+#include <rqt_example_cpp/ui_my_plugin.h>
+#include <QWidget>
+
+namespace rqt_example_cpp {
+
+class MyPlugin
+  : public rqt_gui_cpp::Plugin
+{
+  Q_OBJECT
+public:
+  MyPlugin();
+  virtual void initPlugin(qt_gui_cpp::PluginContext& context);
+  virtual void shutdownPlugin();
+  virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const;
+  virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings);
+
+  // Comment in to signal that the plugin has a way to configure it
+  // bool hasConfiguration() const;
+  // void triggerConfiguration();
+private:
+  Ui::MyPluginWidget ui_;
+  QWidget* widget_;
+};
+}  // namespace rqt_example_cpp
+#endif  // rqt_example_cpp_my_plugin_H
---
+++ rqt_example_cpp/src/rqt_example_cpp/my_plugin.cpp
@@ -0,0 +1,57 @@
+#include "rqt_example_cpp/my_plugin.h"
+#include <pluginlib/class_list_macros.h>
+#include <QStringList>
+
+namespace rqt_example_cpp {
+
+MyPlugin::MyPlugin()
+  : rqt_gui_cpp::Plugin()
+  , widget_(0)
+{
+  // Constructor is called first before initPlugin function, needless to say.
+
+  // give QObjects reasonable names
+  setObjectName("MyPlugin");
+}
+
+void MyPlugin::initPlugin(qt_gui_cpp::PluginContext& context)
+{
+  // access standalone command line arguments
+  QStringList argv = context.argv();
+  // create QWidget
+  widget_ = new QWidget();
+  // extend the widget with all attributes and children from UI file
+  ui_.setupUi(widget_);
+  // add widget to the user interface
+  context.addWidget(widget_);
+}
+
+void MyPlugin::shutdownPlugin()
+{
+  // TODO unregister all publishers here
+}
+
+void MyPlugin::saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const
+{
+  // TODO save intrinsic configuration, usually using:
+  // instance_settings.setValue(k, v)
+}
+
+void MyPlugin::restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings)
+{
+  // TODO restore intrinsic configuration, usually using:
+  // v = instance_settings.value(k)
+}
+
+/*bool hasConfiguration() const
+{
+  return true;
+}
+
+void triggerConfiguration()
+{
+  // Usually used to open a dialog to offer the user a set of configuration
+}*/
+
+} // namespace
+PLUGINLIB_DECLARE_CLASS(rqt_example_cpp, MyPlugin, rqt_example_cpp::MyPlugin, rqt_gui_cpp::Plugin)
