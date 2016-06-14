35ec246458d28c7ff9357422b7acb992acc59a5c
Lucas Walter <wsacul@gmail.com>
2016-06-14 10:55:35 -0700

    roslint compliance

--- rqt_example_cpp/setup.py
+++ rqt_example_cpp/setup.py
@@ -1,4 +1,4 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+# # ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
@@ -11,4 +11,3 @@ setup_args = generate_distutils_setup(
 )
 
 setup(**setup_args)
-
