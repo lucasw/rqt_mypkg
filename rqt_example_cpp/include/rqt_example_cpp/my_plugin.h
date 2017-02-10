2696aa0f12a5328ab3f2bba614021bd3d5fe8637
Lucas Walter <wsacul@gmail.com>
2017-02-10 04:43:16 -0800

    roslint compliance

--- rqt_example_cpp/include/rqt_example_cpp/my_plugin.h
+++ rqt_example_cpp/include/rqt_example_cpp/my_plugin.h
@@ -1,11 +1,15 @@
-#ifndef rqt_example_cpp_my_plugin_H
-#define rqt_example_cpp_my_plugin_H
+/*
+  Copyright 2016 Lucas Walter
+*/
+#ifndef RQT_EXAMPLE_CPP_MY_PLUGIN_H
+#define RQT_EXAMPLE_CPP_MY_PLUGIN_H
 
 #include <rqt_gui_cpp/plugin.h>
 #include <rqt_example_cpp/ui_my_plugin.h>
 #include <QWidget>
 
-namespace rqt_example_cpp {
+namespace rqt_example_cpp
+{
 
 class MyPlugin
   : public rqt_gui_cpp::Plugin
@@ -15,8 +19,10 @@ public:
   MyPlugin();
   virtual void initPlugin(qt_gui_cpp::PluginContext& context);
   virtual void shutdownPlugin();
-  virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const;
-  virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings);
+  virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings,
+      qt_gui_cpp::Settings& instance_settings) const;
+  virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings,
+      const qt_gui_cpp::Settings& instance_settings);
 
   // Comment in to signal that the plugin has a way to configure it
   // bool hasConfiguration() const;
@@ -26,4 +32,4 @@ private:
   QWidget* widget_;
 };
 }  // namespace rqt_example_cpp
-#endif  // rqt_example_cpp_my_plugin_H
+#endif  // RQT_EXAMPLE_CPP_MY_PLUGIN_H
--- rqt_example_cpp/src/rqt_example_cpp/my_plugin.cpp
+++ rqt_example_cpp/src/rqt_example_cpp/my_plugin.cpp
@@ -1,8 +1,13 @@
+/*
+  Copyright 2016 Lucas Walter
+*/
+
 #include "rqt_example_cpp/my_plugin.h"
 #include <pluginlib/class_list_macros.h>
 #include <QStringList>
 
-namespace rqt_example_cpp {
+namespace rqt_example_cpp
+{
 
 MyPlugin::MyPlugin()
   : rqt_gui_cpp::Plugin()
@@ -28,18 +33,18 @@ void MyPlugin::initPlugin(qt_gui_cpp::PluginContext& context)
 
 void MyPlugin::shutdownPlugin()
 {
-  // TODO unregister all publishers here
+  // unregister all publishers here
 }
 
-void MyPlugin::saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const
+void MyPlugin::saveSettings(qt_gui_cpp::Settings& plugin_settings,
+    qt_gui_cpp::Settings& instance_settings) const
 {
-  // TODO save intrinsic configuration, usually using:
   // instance_settings.setValue(k, v)
 }
 
-void MyPlugin::restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings)
+void MyPlugin::restoreSettings(const qt_gui_cpp::Settings& plugin_settings,
+    const qt_gui_cpp::Settings& instance_settings)
 {
-  // TODO restore intrinsic configuration, usually using:
   // v = instance_settings.value(k)
 }
 
@@ -53,5 +58,5 @@ void triggerConfiguration()
   // Usually used to open a dialog to offer the user a set of configuration
 }*/
 
-} // namespace
+}  // namespace rqt_example_cpp
 PLUGINLIB_DECLARE_CLASS(rqt_example_cpp, MyPlugin, rqt_example_cpp::MyPlugin, rqt_gui_cpp::Plugin)
