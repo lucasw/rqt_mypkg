6e9d1b7b84b3abc4c57f5b03382a274744890ae3
Lucas Walter <wsacul@gmail.com>
2021-07-24 10:08:22 -0700

    Upgrade script to python3

--- rqt_example_py/src/rqt_mypkg/my_module.py
+++ rqt_example_py/src/rqt_mypkg/my_module.py
@@ -23,8 +23,8 @@ class MyPlugin(Plugin):
                       help="Put plugin in silent mode")
         args, unknowns = parser.parse_known_args(context.argv())
         if not args.quiet:
-            print 'arguments: ', args
-            print 'unknowns: ', unknowns
+            print('arguments: ', args)
+            print('unknowns: ', unknowns)
 
         # Create QWidget
         self._widget = QWidget()
